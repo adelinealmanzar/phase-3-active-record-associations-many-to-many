6a2bb8a59ef1e2447c4b6ef95985abf984dcfc37
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-28 07:53:49 -0800

    commit tree c01db54aaccb230f5539a5657a5843de470fc73f

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews #allows for Game.first.reviews
+  has_many :users, through: :reviews #allows for Game.first.users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  
+    has_many :reviews #allows for: User.first.reviews
+    has_many :games, through: :reviews #allows for User.first.games
 end
